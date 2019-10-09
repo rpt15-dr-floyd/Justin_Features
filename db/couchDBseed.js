5bb50fb4871a3d088fd067b8435ab105b109b3c6
Justin O'Hara <justinrobertohara@gmail.com>
2019-10-08 19:30:14 -0700

    couchDB 10m seed script

---
+++ db/couchDBseed.js
@@ -0,0 +1,47 @@
+const nano = require('nano')('http://localhost:5984');
+const faker = require('faker');
+
+const features = nano.db.use('features');
+
+const seedCouchDB = (start, numOfDocs) => {
+  if (start < numOfDocs / 1000) {
+    let bulkData = [];
+    for (let i = 0; i < 1000; i++) {
+      bulkData.push({
+        aboutHeader: faker.lorem.sentence(),
+        aboutBody: faker.lorem
+          .paragraph(
+            (nb_sentences = 1),
+            (variable_nb_sentences = true),
+            (ext_word_list = null)
+          )
+          .slice(0, 250),
+        featureTitle: faker.lorem.sentence(
+          (nb_words = 3),
+          (variable_nb_words = true),
+          (ext_word_list = null)
+        ),
+        features: faker.lorem
+          .paragraphs(
+            (nb_sentences = 1),
+            (variable_nb_sentences = true),
+            (ext_word_list = null)
+          )
+          .slice(0, 250)
+      });
+    }
+
+    features
+      .bulk({ docs: bulkData })
+      .then(() => {
+        start++;
+        console.log(`you have inserted ${start * 1000}`);
+        seedCouchDB(start, numOfDocs);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+
+seedCouchDB(0, 10000000);
