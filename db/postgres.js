54ee537d0ad1f5bd85da79745198abb9153ceb6c
Justin O'Hara <justinrobertohara@gmail.com>
2019-10-25 17:07:42 -0700

    added psql ec2 instance

--- db/postgres.js
+++ db/postgres.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('feature', 'postgres', 'root', {
-  host: 'localhost',
+const sequelize = new Sequelize('feature', 'power_user', 'root', {
+  host: 'ec2-18-191-197-10.us-east-2.compute.amazonaws.com',
   dialect: 'postgres',
   logging: false
 });
@@ -15,6 +15,12 @@ sequelize
     console.log(err);
   });
 
+sequelize.sync({
+  force: true
+}).then(()=>{
+  console.log('created tables if they do not exist')
+});
+
 const Game = sequelize.define('game', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   aboutHeader: { type: Sequelize.STRING },
@@ -22,7 +28,6 @@ const Game = sequelize.define('game', {
   featureTitle: { type: Sequelize.STRING },
   features: { type: Sequelize.STRING }
 });
-
 
 module.exports = {
   Game
